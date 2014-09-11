e696be26cc87caffcaf4e7495083f48a2b54c0f0
Sean Gavan <c16sean.gavan@usafa.edu>
2014-09-10 20:57:22 -0600

    code update

--- code/Calculator.asm
+++ code/Calculator.asm
@@ -5,7 +5,7 @@
 ; Section: T5A
 ; Project: Lab 1 for ECE 382 - an assembly calculator
 ; Date started: 9 Sept. 14
-; Date last changed: 9 Sept. 14
+; Date last changed: 10 Sept. 14
 ;-------------------------------------------------------------------------------
             .cdecls C,LIST,"msp430.h"       ; Include device header file
 
@@ -23,16 +23,6 @@ myProgram:      .byte      0x13,0x22,0x14,0x11,0x37
                 .data
 myResults:      .space      20
 
-ADD_OP:		.equ	0x11
-
-SUB_OP:		.equ	0x22
-
-MUL_OP:		.equ	0x33
-
-CLR_OP:		.equ	0x44
-
-END_OP:		.equ	0x55
-
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
@@ -40,22 +30,63 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
                                             ; Main loop here
 ;-------------------------------------------------------------------------------
 readers:
-			mov.w	#store, r10
+			mov.w	#myResults, r10
 			mov.w	#myProgram, r5
 			mov.b	@r5+, r6
 			mov.b	@r5+, r7
 			mov.b	@r5+, r8
+			mov.b	@r5+, r12
+			mov.b	@r5+, r13
 
-checkADD:	cmp.b	#0x11, r7
+checks:
+			cmp.b	#0x11, r7
+			jeq		checkADD
+			cmp.b	#0x22, r7
+			jeq		checkSUB
+			;cmp.b	#0x33, r7
+			;jeq		checkMUL
+			;cmp.b	#0x44, r7
+			;jeq		checkCLR
+			;cmp.b	#0x55, r7
+			;jeq		checkEND
+			jmp		NOOP
 
-checkSUB:	cmp.b	#0x22, r7
+checkADD:
+			mov.b	r6, r9
+			add.b	r8, r9
+			jmp		storeAnswer
 
-checkMUL:	cmp.b	#0x33, r7
+checkSUB:
+			mov.b	r6, r9
+			sub.b	r8, r9
+			jmp		storeAnswer
 
-checkCLR:	cmp.b	#0x44, r7
+;checkMUL:
 
-checkEND:	cmp.b	#0x55, r7
+			;jmp		storeAnswer
 
+;checkCLR:
+
+
+
+storeAnswer:
+			mov.b	r9, 0(r10)
+			inc.b	r10
+			jmp		next
+
+next:
+			cmp.b	#0x55, r12
+			jeq		checkEND
+			mov.b	r9, r6
+			mov.b	r12, r7
+			mov.b	r13, r8
+			jmp		checks
+
+checkEND:
+			jmp		checkEND
+
+NOOP:
+			jmp		NOOP
 
 ;-------------------------------------------------------------------------------
 ;           Stack Pointer definition
