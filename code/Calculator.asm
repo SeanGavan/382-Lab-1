5c4e5a9f41f1aa3de59047931c6f7fb1b2f28139
Sean Gavan <c16sean.gavan@usafa.edu>
2014-09-10 22:18:05 -0600

    code w/ B Functionality

--- code/Calculator.asm
+++ code/Calculator.asm
@@ -18,7 +18,7 @@
                                             ; section
 ;-------------------------------------------------------------------------------
                 .text
-myProgram:      .byte      0x13,0x22,0x14,0x11,0x37
+myProgram:      .byte      0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0xDD, 0x44, 0x08, 0x22, 0x09, 0x44, 0xFF, 0x22, 0xFD, 0x55
 
                 .data
 myResults:      .space      20
@@ -35,46 +35,65 @@ readers:
 			mov.b	@r5+, r6
 			mov.b	@r5+, r7
 			mov.b	@r5+, r8
-			mov.b	@r5+, r12
-			mov.b	@r5+, r13
 
 checks:
+			mov.b	@r5+, r12
+			mov.b	@r5+, r13
 			cmp.b	#0x11, r7
 			jeq		checkADD
 			cmp.b	#0x22, r7
 			jeq		checkSUB
 			;cmp.b	#0x33, r7
 			;jeq		checkMUL
-			;cmp.b	#0x44, r7
-			;jeq		checkCLR
-			;cmp.b	#0x55, r7
-			;jeq		checkEND
+			cmp.b	#0x55, r7
+			jeq		checkEND
 			jmp		NOOP
 
 checkADD:
 			mov.b	r6, r9
-			add.b	r8, r9
+			add.w	r8, r9
+			cmp.w	#0x00FF, r9
+			jge		overMax
+			jmp		storeAnswer
+
+overMax:
+			mov.b	#0xFF, r9
 			jmp		storeAnswer
 
 checkSUB:
 			mov.b	r6, r9
-			sub.b	r8, r9
+			sub.w	r8, r9
+			cmp.w	#0x0000, r9
+			jl		underMin
+			jmp		storeAnswer
+
+underMin:
+			mov.b	#0x00, r9
 			jmp		storeAnswer
 
 ;checkMUL:
 
 			;jmp		storeAnswer
 
-;checkCLR:
-
+checkCLR:
+			mov.b	@r5+, r14
+			mov.b	@r5+, r15
+			mov.b	r13, r6
+			mov.b	r14, r7
+			mov.b	r15, r8
+			mov.b	#0x00, 0(r10)
+			inc.w	r10
+			jmp		checks
 
 
 storeAnswer:
 			mov.b	r9, 0(r10)
-			inc.b	r10
+			inc.w	r10
 			jmp		next
 
 next:
+			cmp.b	#0x44, r12
+			jeq		checkCLR
 			cmp.b	#0x55, r12
 			jeq		checkEND
 			mov.b	r9, r6
